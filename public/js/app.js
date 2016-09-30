785b6e0479baf80d92edbe435e29e96c061c7fa7
Ram Pasala <ram.pasala7@gmail.com>
2016-09-30 16:08:12 +0530

    pushing state controller changes

--- public/js/app.js
+++ public/js/app.js
@@ -11,18 +11,73 @@ app.config(function ($routeProvider){
         templateUrl:"/views/homePage.html" 
     })
     .when ('/states', {
-        templateUrl:"/views/states.html" 
+        templateUrl:"/views/states.html", 
+        controller:"stateController"
     })
     .when ('/guestbook', {
         templateUrl:"/views/guestBook.html" 
-    });
+    })
+    .otherwise({
+        redirectTo:'/'
+    })
 });
 
-app.controller('loginController',function ($scope,$location,$http) {
-  
+app.controller('loginController',function ($scope,$location,$http,$rootScope) {
+  $rootScope.logIn = false ;
+    $scope.login = function() {
+                     var params = {
+                        "user": $scope.username,
+                        "password": $scope.password
+                      };
+                    
+                    $http({
+                        url: '/login',
+                        method: 'POST',
+                        data: params}).success(function(data) {
+                        if (data.result) {
+                            $scope.loginerror = "success!";
+                            $rootScope.logIn = true;
+                            window.location.href = "/#home";
+                        } else {
+                            $scope.loginerror = "Incorrect username or password!";
+                        }
+                    }).error(function(data) {
+                        $scope.loginerror = "Error in server!";
+                    });
+                }
 
+          
+  
 });
 app.controller('stateController',function ($scope,$http) {
 
+$scope.search = function() {
+   // var searchState = $scope.searchState;
+   // var states = [];
+    $http({
+        url: '/states',
+        method: 'GET',
+    }).success(function (data) {
+        console.log(data);
+        $scope.searchData = data;
+
+    }).error(function(data) {
+        $scope.loginerror = "Error in server!";
+    });
+}
+
+$scope.abbreviations = function () {
+
+ $http({
+        url: '/states/abbreviations',
+        method: 'GET',
+    }).success(function (data) {
+        console.log(data);
+        $scope.searchAbbreviation = data;
+
+    }).error(function(data) {
+        $scope.loginerror = "Error in server!";
+    });
+}
 
 });
